4d179a21325841e68311356309709a643dd57e03
yves <thirion.yves@gmail.com>
2021-12-06 14:05:36 +0100

    Created and displayed cube

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { GreaterStencilFunc } from "three";
 
 const canvas = document.querySelector(".webgl"); // App root
 const scene = new THREE.Scene(); // Scene of the action
@@ -12,10 +13,23 @@ const camera = new THREE.PerspectiveCamera( // Set camera
 // Positionning camera in X Y Z
 camera.position.y = 0;
 camera.position.x = 0;
-camera.position.z = 0;
+camera.position.z = 2;
 
 // Add camera to the scene
 scene.add(camera);
+
+// Geometry
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Material
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+});
+
+// Assemble cube
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+scene.add(cube);
 
 // Instantiate renderer with canvas
 const renderer = new THREE.WebGLRenderer({
