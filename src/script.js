7b8984ab0df770c85aa9d3a01c4d60de8594fd9d
yves <thirion.yves@gmail.com>
2021-12-06 13:58:54 +0100

    Setting three.js canvas, scene, camera, clock

---
+++ src/script.js
@@ -0,0 +1,36 @@
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.querySelector(".webgl"); // App root
+const scene = new THREE.Scene(); // Scene of the action
+const camera = new THREE.PerspectiveCamera( // Set camera
+    75, // FOV
+    window.innerWidth / window.innerHeight, // Ratio
+    0.1, // Closest distance allowed for camera
+    150 // Farest distance allowed for camera
+);
+// Positionning camera in X Y Z
+camera.position.y = 0;
+camera.position.x = 0;
+camera.position.z = 0;
+
+// Add camera to the scene
+scene.add(camera);
+
+// Instantiate renderer with canvas
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+
+// Update canvas size
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick); // Call tick every frame
+};
+
+tick(); // First call of tick
